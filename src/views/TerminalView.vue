82d7f37bb0f6caadf9de9325509acf0a2065368e
Moe Dayraki <mohammeddayraki@hotmail.com>
2022-05-20 20:42:42 +0000

    started working on the terminal part

--- src/views/TerminalView.vue
+++ src/views/TerminalView.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is the terminal page</h1>
+  <div class="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base">
+    <div className="p-8 overflow-hidden h-full border-2 rounded border-light-yellow dark:border-dark-yellow">
+        <!-- <div ref={containerRef} className="overflow-y-auto h-full">
+          <History history={history} />
+
+          <Input
+            inputRef={inputRef}
+            containerRef={containerRef}
+            command={command}
+            history={history}
+            lastCommandIndex={lastCommandIndex}
+            setCommand={setCommand}
+            setHistory={setHistory}
+            setLastCommandIndex={setLastCommandIndex}
+            clearHistory={clearHistory}
+          />
+        </div> -->
+      </div>
   </div>
 </template>
+
+
 
