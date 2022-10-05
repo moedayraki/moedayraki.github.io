98bec47cc48c5247c8bcbc54c826668bfd7aca63
Moe Dayraki <mohammeddayraki@hotmail.com>
2022-10-05 19:19:19 +0000

    added the particle js to the project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Particles from "vue3-particles";
 
 // import "primeflex/primeflex.css";
 // import "primevue/resources/themes/tailwind-light/theme.css";
@@ -15,5 +16,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Particles)
 
 app.mount('#app')
